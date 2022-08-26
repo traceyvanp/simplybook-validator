25b95c99e63fb76948dabed10362ec3e096141ac
traceyvanp <traceyvanp@gmail.com>
2022-08-26 13:55:27 +0200

    testing again

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 include_once 'ExternalValidatorException.php';
 include_once 'ExternalValidator.php';
 
-$incomingData = json_decode(file_get_contents('php://input/booking'),true);
-$incomingData = $incomingData['current_booking'];
+$incomingData = json_decode(file_get_contents('php://input/'),true);
+//$incomingData = $incomingData['current_booking'];
+echo $incomingData->{'booking'};
 
 /*
 //uncomment for local testing
 $incomingData = json_decode(file_get_contents('booking.json'),true);
 $incomingData = $incomingData['current_booking'];
-*/
 
 if($incomingData['service_id'] === "14"){
     echo json_encode(array());
@@ -20,5 +20,5 @@ if($incomingData['service_id'] === "14"){
     $result = $validator->validate($incomingData);
     echo json_encode($result);
 }
-
+*/
 ?>
