28235b85c253e551b4537a1c3729359b367ad6d4
traceyvanp <traceyvanp@gmail.com>
2022-08-26 12:46:20 +0200

    test with error

--- index.php
+++ index.php
@@ -10,27 +10,20 @@ $childs = implode("", $incomingData);
 echo $childs;
 echo "are you here??"
 */
-console_log($incomingData);
-
-function console_log($output, $with_script_tags = true) {
-    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
-');';
-    if ($with_script_tags) {
-        $js_code = '<script>' . $js_code . '</script>';
-    }
-    echo $js_code;
-}
 
 //uncomment for local testing
 //$incomingData = json_decode(file_get_contents('booking.json'),true);
 
-/*
+
 if($incomingData['service_id'] == "14"){
     echo json_encode(array());
 } else {
+    echo "Some Error";
+/*
     $validator = new ExternalValidator();
     $result = $validator->validate($incomingData);
     echo json_encode($result);
+    */
 }
-*/
+
 ?>
