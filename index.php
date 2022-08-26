1227524b4abd9db4eccd46f4efea5721a848e21c
traceyvanp <traceyvanp@gmail.com>
2022-08-26 13:04:08 +0200

    test with decode booking

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 include_once 'ExternalValidatorException.php';
 include_once 'ExternalValidator.php';
 
-$incomingData = json_decode(file_get_contents('php://input/booking'), true);
+$incomingData = file_get_contents('php://input/booking');
+$incomingData = json_decode($incomingData['booking'], true);
 /*
 $childs = implode("", $incomingData);
 echo $childs;
@@ -14,17 +15,12 @@ echo "are you here??"
 //uncomment for local testing
 //$incomingData = json_decode(file_get_contents('booking.json'),true);
 
-if(!$incomingData){
-//if($incomingData['service_id'] == "14"){
-    echo $incomingData;
-    //echo json_encode(array());
+if($incomingData['service_id'] == "14"){
+    echo json_encode(array());
 } else {
-    echo $incomingData;
-/*
     $validator = new ExternalValidator();
     $result = $validator->validate($incomingData);
     echo json_encode($result);
-    */
 }
 
 ?>
