54a7489214859407bb581fd56a2a92a2577d084f
traceyvanp <traceyvanp@gmail.com>
2022-08-26 11:32:39 +0200

    testing with simplybook index.php file

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php 
-echo "Hello Tracey";
+<?php
+
+
+include_once 'ExternalValidatorException.php';
+include_once 'ExternalValidator.php';
+
+echo "are you here?";
+/*
+$incomingData = file_get_contents('php://input');
+$obj = json_decode($incomingData, true);
+$childs = implode("", $obj);
+echo $childs;
+echo "are you here??"
+
+
+//uncomment for local testing
+//$incomingData = json_decode(file_get_contents('booking.json'),true);
+
+/*
+if($incomingData['service_id'] == "14"){
+    echo json_encode(array());
+} else {
+    $validator = new ExternalValidator();
+    $result = $validator->validate($incomingData);
+    echo json_encode($result);
+}
+*/
 ?>
