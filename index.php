c245810c0722b443c0ecfc47cd65d52cfe9d0463
traceyvanp <traceyvanp@gmail.com>
2022-08-26 12:27:18 +0200

    test with console log function

--- index.php
+++ index.php
@@ -5,18 +5,26 @@ include_once 'ExternalValidatorException.php';
 include_once 'ExternalValidator.php';
 
 $incomingData = json_decode(file_get_contents('php://input'), true);
-
 /*
-$obj = json_decode($incomingData, true);
-$childs = implode("", $obj);
+$childs = implode("", $incomingData);
 echo $childs;
 echo "are you here??"
 */
+console_log($incomingData);
+
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
 
 //uncomment for local testing
 //$incomingData = json_decode(file_get_contents('booking.json'),true);
 
-
+/*
 if($incomingData['service_id'] == "14"){
     echo json_encode(array());
 } else {
@@ -24,5 +32,5 @@ if($incomingData['service_id'] == "14"){
     $result = $validator->validate($incomingData);
     echo json_encode($result);
 }
-
+*/
 ?>
