8321f0b5294f10446a9cbec7b8b4fb8b26d972a9
traceyvanp <traceyvanp@gmail.com>
2022-09-18 12:44:39 +0200

    edited

--- index.php
+++ index.php
@@ -4,15 +4,23 @@
 include_once 'ExternalValidatorException.php';
 include_once 'ExternalValidator.php';
 
-$incomingData = json_decode(file_get_contents('https://ozarkwings.simplybook.me/v2/booking/'),true);
+$incomingData = json_decode(file_get_contents('php://input'),true);
 //$incomingData = $incomingData['current_booking'];
-echo $incomingData;
+//echo $incomingData;
+echo $incomingData['service_id'];
+
+if(!$incomingData){
+    echo json_encode(array());
+} else {
+    $validator = new ExternalValidator();
+    $result = $validator->validate($incomingData);
+    echo json_encode($result);
+}
 
 /*
 //uncomment for local testing
 $incomingData = json_decode(file_get_contents('booking.json'),true);
 $incomingData = $incomingData['current_booking'];
-
 if($incomingData['service_id'] === "14"){
     echo json_encode(array());
 } else {
@@ -21,4 +29,5 @@ if($incomingData['service_id'] === "14"){
     echo json_encode($result);
 }
 */
+
 ?>
