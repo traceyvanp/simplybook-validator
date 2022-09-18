c0958159e78dbae041ece128fce0f2ccbd6dd45f
traceyvanp <traceyvanp@gmail.com>
2022-09-18 21:51:01 +0200

    add php test file

--- index.php
+++ index.php
@@ -1,35 +1,3 @@
-<?php
-
-file_put_contents('test.dat', file_get_contents('php://input'));
-echo("This is a test.");
-
-include_once 'ExternalValidatorException.php';
-include_once 'ExternalValidator.php';
-
-$incomingData = json_decode(file_get_contents('php://input'),true);
-//$incomingData = $incomingData['current_booking'];
-//echo $incomingData;
-echo $incomingData['service_id'];
-
-if(!$incomingData){
-    echo json_encode(array());
-} else {
-    $validator = new ExternalValidator();
-    $result = $validator->validate($incomingData);
-    echo json_encode($result);
-}
-
-/*
-//uncomment for local testing
-$incomingData = json_decode(file_get_contents('booking.json'),true);
-$incomingData = $incomingData['current_booking'];
-if($incomingData['service_id'] === "14"){
-    echo json_encode(array());
-} else {
-    $validator = new ExternalValidator();
-    $result = $validator->validate($incomingData);
-    echo json_encode($result);
-}
-*/
-
+<?php 
+echo "Hello Tracey";
 ?>
