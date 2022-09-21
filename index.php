6448ff8c705c4a1c481eab020b81490f72db9ffe
traceyvanp <traceyvanp@gmail.com>
2022-09-21 13:28:34 +0200

    removed test

--- index.php
+++ index.php
@@ -3,32 +3,25 @@
 include_once 'ExternalValidatorException.php';
 include_once 'ExternalValidator.php';
 
-/*
-$incomingData = json_decode(file_get_contents('php://input'),true);
-//$incomingData = $incomingData['current_booking'];
-//echo $incomingData;
 
-if(!$incomingData){
-    //echo json_encode(array());
-    echo $incomingData;
-    echo "testing";
+$incomingData = json_decode(file_get_contents('php://input'),true);
+if($incomingData['service_id'] === "14"){
+    echo json_encode(array());
 } else {
     $validator = new ExternalValidator();
     $result = $validator->validate($incomingData);
     echo json_encode($result);
 }
 
-*/
-
-//uncomment for local testing
+/* uncomment for local testing
 $incomingData = json_decode(file_get_contents('booking.json'),true);
 if($incomingData['service_id'] === "14"){
     echo json_encode(array());
 } else {
-    echo "here";
     $validator = new ExternalValidator();
     $result = $validator->validate($incomingData);
     echo json_encode($result);
 }
+*/
 
 ?>
