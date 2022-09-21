c328565f47272d16d265bbc606df1c9c0cf8d343
traceyvanp <traceyvanp@gmail.com>
2022-09-21 13:08:59 +0200

    updated composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4f0f60792c0ce3be1a3690b1dadfdf9e3fca2733',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4f0f60792c0ce3be1a3690b1dadfdf9e3fca2733',
+            'dev_requirement' => false,
+        ),
+    ),
+);
