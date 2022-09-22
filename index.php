84e15e32c7e8645a076068fe0a781ee95af76d42
traceyvanp <traceyvanp@gmail.com>
2022-09-22 15:07:13 +0200

    this is a test

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 include_once 'ExternalValidatorException.php';
 include_once 'ExternalValidator.php';
 
-$incomingData = json_decode(file_get_contents('php://input'),true);
+//$incomingData = json_decode(file_get_contents('php://input'),true);
 //uncomment below for local testing
-//$incomingData = json_decode(file_get_contents('booking.json'),true);
+$incomingData = json_decode(file_get_contents('booking.json'),true);
 
-//temp test
+//the service is lodging (id 14) then don't run the validation
 if($incomingData['service_id'] === "14"){
-    echo json_encode(array("error" => "this works"));
+    echo json_encode(array('error' => 'this works'));
 } else {
     $validator = new ExternalValidator();
     $result = $validator->validate($incomingData);
