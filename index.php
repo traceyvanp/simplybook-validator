0a2cea7a7c697cfa6a308f49d1912696e3d639b0
traceyvanp <traceyvanp@gmail.com>
2022-09-23 22:39:37 +0200

    test

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 
+//require('../vendor/autoload.php');
 include_once 'ExternalValidatorException.php';
 include_once 'ExternalValidator.php';
+
 
 $incomingData = json_decode(file_get_contents('php://input'),true);
 //uncomment below for local testing
 //$incomingData = json_decode(file_get_contents('booking.json'),true);
-print("Hello Tracey");
-print_r($incomingData);
+print(json_encode($incomingData));
 
+/*
 //the service is lodging (id 14) then don't run the validation
 if($incomingData['service_id'] === '14'){
     echo json_encode(array());
@@ -17,6 +19,5 @@ if($incomingData['service_id'] === '14'){
     $result = $validator->validate($incomingData);
     echo json_encode($result);
 }
-
-
+*/
 ?>
