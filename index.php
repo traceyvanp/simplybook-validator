b2e4185f7aaffbceb9129f63bcc926411db41604
traceyvanp <traceyvanp@gmail.com>
2023-02-21 21:49:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,11 @@
 <?php
 
-$response = get_web_page("https://ozarkwings.com/simplybook.me/v2/booking");
-$resArr = array();
-$resArr = json_decode($response);
-#echo "<pre>"; print_r($resArr); echo "</pre>";
-print_r(json_encode($resArr));
+include_once 'ExternalValidatorException.php';
+include_once 'ExternalValidator.php';
 
-function get_web_page($url) {
-    $options = array(
-        CURLOPT_RETURNTRANSFER => true,   // return web page
-        CURLOPT_HEADER         => false,  // don't return headers
-        CURLOPT_FOLLOWLOCATION => true,   // follow redirects
-        CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
-        CURLOPT_ENCODING       => "",     // handle compressed
-        CURLOPT_USERAGENT      => "test", // name of client
-        CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
-        CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
-        CURLOPT_TIMEOUT        => 120,    // time-out on response
-    ); 
 
-    $ch = curl_init($url);
-    curl_setopt_array($ch, $options);
+$incomingData = json_decode(file_get_contents('php://input'),true);
 
-    $content  = curl_exec($ch);
-
-    curl_close($ch);
-
-    return $content;
-}
+print(json_encode($incomingData["client_id"]));
 
 ?>
