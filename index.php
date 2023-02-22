a91993a10325b69950d7133e6e6cb741e111d9ce
traceyvanp <traceyvanp@gmail.com>
2023-02-22 11:55:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,5 @@ $incomingData = json_decode(file_get_contents('php://input'),true);
 if(!$incomingData){
     echo json_encode(array());
 } else {
-    $arr = array('error' => array($incomingData['service_id']));
-    echo json_encode($arr);
+    echo '{"errors":["This is your customized error."]}';
 }
